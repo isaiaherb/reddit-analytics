93d1ea91259412a8d158fc8e893e6d952b2f9200
isaiaherb <146012975+isaiaherb@users.noreply.github.com>
2024-06-06 13:44:50 -0600

    Update finbert_sentiment_analysis.py

--- airflow-docker/dags/finbert_sentiment_analysis.py
+++ airflow-docker/dags/finbert_sentiment_analysis.py
@@ -1,4 +1,4 @@
-# Compute FinBERT sentiment scores for the comments and submissions in SQL Server.
+# Compute positive, negative, neutral, and compound FinBERT sentiment scores for the comments and submissions in SQL Server.
 import pandas as pd
 from sqlalchemy import create_engine, text
 import urllib
